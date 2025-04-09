714d74f9a03a52d710dd91516e79f67bce930a2d
Aiden Makovicka <apmak4@gmail.com>
2025-04-09 00:08:00 -0500

    Added Product Constructor and Checkout Model

---
+++ OnlineShoppingApp/OnlineShoppingApp/Models/Checkout.cs
@@ -0,0 +1,17 @@
+public class Checkout
+{
+    public int OrderId { get; }
+    public User User { get; }
+    public List<Product> Items { get; } 
+    public DateOnly Date { get; }
+
+    public Checkout() { }
+
+    public Checkout(int orderId,User user, List<Product> items, DateOnly date){
+        this.OrderId = orderId;
+        this.User = user;
+        this.Items = items;
+        this.Date = date;
+    }
+
+}
--- OnlineShoppingApp/OnlineShoppingApp/Models/Product.cs
+++ OnlineShoppingApp/OnlineShoppingApp/Models/Product.cs
@@ -1,6 +1,7 @@
 
 public class Product
 {
+
     public int Id { get; }
     public String Name { get; }
     // public List<Image> Images { get; }
@@ -11,4 +12,17 @@ public class Product
     public Double Rating { get; }
     public String Sku { get; }
     // public Category category { get; }
+
+    public Product() {} 
+
+    public Product(int id, String name, String manInfo, String description, Double weight, Double rating, String sku){
+        this.Id = id;
+        this.Name = name;
+        this.ManInfo = manInfo;
+        this.Description = description;
+        this.Weight = weight;
+        this.Rating = rating;
+        this.Sku = sku;
+    }
+
 }
